de796cecf065b5ff6d471c683f2d895611baf17b
Vasilij Schneidermann <mail@vasilij.de>
2019-12-24 23:52:24 +0100

    Introduce endianness PyEnums (#13)

--- python/asm.h
+++ python/asm.h
@@ -6,6 +6,8 @@
 #include <r_asm.h>
 #include "common.h"
 
+void py_export_asm_enum(PyObject *tp_dict);
+
 void Radare_plugin_asm_free(RAsmPlugin *ap);
 
 PyObject *Radare_plugin_asm(Radare* self, PyObject *args);
