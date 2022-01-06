706631e9d5831e044999dd2bba6d7a498427f899
pancake <pancake@nopcode.org>
2022-01-06 22:43:57 +0100

    Lot of improvements in qjs

--- quickjs/build.mk
+++ quickjs/build.mk
@@ -9,18 +9,18 @@ ifeq ($(QJS_BRANCH),frida)
 	QJS_CFLAGS+=-DQJS_NAME="\\"$(QJS_NAME)\\""
 	QJS_CFLAGS+=-DQJS_FRIDA=1
 else
-	QJS_CFLAGS+=-DQJS_FRIDA=0
 	QJS_NAME=quickjs-bellard
 	QJS_GITURL=https://github.com/bellard/quickjs
 	QJS_CFLAGS+=-DQJS_NAME="\\"$(QJS_NAME)\\""
+	QJS_CFLAGS+=-DQJS_FRIDA=0
 endif
 
 QJS_CFLAGS+=-DQJS_LIBC=$(QJS_LIBC)
 
-QJS_LIBS+=-lr_core -lr_config
+QJS_LIBS+=-lr_core -lr_config -lr_cons
 
 lang_quickjs.${EXT_SO}: $(QJS_NAME)
-	$(MAKE) -C $(QJS_NAME) libquickjs.a
+	$(MAKE) CFLAGS_OPT="-Oz -DCONFIG_VERSION=\\\"0.1\\\"" -C $(QJS_NAME) libquickjs.a
 	-$(CC) -flto -Wl,-exported_symbols_list,symbols.lst \
 		-Oz -Wl,-dead_strip -std=c99 $(DUK_CFLAGS) -I quickjs $(QJS_CFLAGS) $(CFLAGS) -fPIC $(LDFLAGS_LIB) \
 		-o lang_quickjs.$(EXT_SO) quickjs.c $(QJS_NAME)/libquickjs.a $(QJS_LIBS)
@@ -28,3 +28,6 @@ lang_quickjs.${EXT_SO}: $(QJS_NAME)
 $(QJS_NAME):
 	git clone $(QJS_GITURL) $(QJS_NAME)
 
+
+o:
+	make clean && make && make user-install
--- quickjs/quickjs.c
+++ quickjs/quickjs.c
@@ -1,4 +1,4 @@
-/* radare - LGPL - Copyright 2020-2021 pancake */
+/* radare - LGPL - Copyright 2020-2022 pancake */
 
 #define _XOPEN_SOURCE 
 #include <stdio.h>
@@ -23,19 +23,17 @@ static bool is_init = false;
 static JSContext *ctx = NULL;
 static JSRuntime *rt = NULL;
 
-static void js_dump_obj(JSContext *ctx, FILE *f, JSValueConst val)
-{
-    const char *str;
-
-    str = JS_ToCString(ctx, val);
-    if (str) {
-        fprintf(f, "%s\n", str);
-        JS_FreeCString(ctx, str);
-    } else {
-        fprintf(f, "[exception]\n");
-    }
+static void js_dump_obj(JSContext *ctx, FILE *f, JSValueConst val) {
+	const char *str = JS_ToCString(ctx, val);
+	if (str) {
+		fprintf (f, "%s\n", str);
+		JS_FreeCString (ctx, str);
+	} else {
+		fprintf (f, "[exception]\n");
+	}
 }
 
+////////////////////////////////
 #if QJS_LIBC
 
 #if QJS_FRIDA
@@ -49,36 +47,35 @@ static void js_dump_obj(JSContext *ctx, FILE *f, JSValueConst val)
 #include "quickjs-frida/quickjs.h"
 
 
-static void js_std_dump_error1(JSContext *ctx, JSValueConst exception_val)
-{
-    JSValue val;
-    bool is_error;
+static void js_std_dump_error1(JSContext *ctx, JSValueConst exception_val) {
+	JSValue val;
+	bool is_error;
 
-    is_error = JS_IsError(ctx, exception_val);
-    js_dump_obj(ctx, stderr, exception_val);
-    if (is_error) {
-        val = JS_GetPropertyStr(ctx, exception_val, "stack");
-        if (!JS_IsUndefined(val)) {
-            js_dump_obj(ctx, stderr, val);
-        }
-        JS_FreeValue(ctx, val);
-    }
+	is_error = JS_IsError(ctx, exception_val);
+	js_dump_obj(ctx, stderr, exception_val);
+	if (is_error) {
+		val = JS_GetPropertyStr (ctx, exception_val, "stack");
+		if (!JS_IsUndefined (val)) {
+			js_dump_obj (ctx, stderr, val);
+		}
+		JS_FreeValue (ctx, val);
+	}
 }
-void js_std_dump_error(JSContext *ctx)
-{
-    JSValue exception_val;
-    
-    exception_val = JS_GetException(ctx);
-    js_std_dump_error1(ctx, exception_val);
-    JS_FreeValue(ctx, exception_val);
+
+void js_std_dump_error(JSContext *ctx) {
+	JSValue exception_val;
+	exception_val = JS_GetException (ctx);
+	js_std_dump_error1 (ctx, exception_val);
+	JS_FreeValue (ctx, exception_val);
 }
 
 #endif
+////////////////////////////////
 
 static JSValue r2log(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
 	size_t plen;
-	const char *n = JS_ToCStringLen2(ctx, &plen, argv[0], false);
-	eprintf ("%s\n", n);
+	const char *n = JS_ToCStringLen2 (ctx, &plen, argv[0], false);
+	r_cons_printf ("%s\n", n);
 	return JS_NewBool (ctx, true);
 }
 
@@ -90,39 +87,68 @@ static JSValue r2cmd(JSContext *ctx, JSValueConst this_val, int argc, JSValueCon
 }
 
 static const JSCFunctionListEntry js_r2_funcs[] = {
-	JS_CFUNC_DEF("cmd", 1, r2cmd),
-	JS_CFUNC_DEF("log", 1, r2log),
+	JS_CFUNC_DEF ("cmd", 1, r2cmd),
+	JS_CFUNC_DEF ("log", 1, r2log),
 };
 
 static int js_r2_init(JSContext *ctx, JSModuleDef *m) {
-	return JS_SetModuleExportList(ctx, m, js_r2_funcs, countof (js_r2_funcs));
+	return JS_SetModuleExportList (ctx, m, js_r2_funcs, countof (js_r2_funcs));
+}
+
+static JSContext *JS_NewCustomContext(JSRuntime *rt) {
+	JSContext *ctx = JS_NewContext (rt);
+	if (!ctx)
+		return NULL;
+#ifdef CONFIG_BIGNUM
+	if (bignum_ext) {
+		JS_AddIntrinsicBigFloat(ctx);
+		JS_AddIntrinsicBigDecimal(ctx);
+		JS_AddIntrinsicOperators(ctx);
+		JS_EnableBignumExt(ctx, TRUE);
+	}
+#endif
+#if QJS_LIBC
+	/* system modules */
+	js_init_module_std(ctx, "std");
+	js_init_module_os(ctx, "os");
+#if 0
+	// if load_std
+	const char *str = "import * as std from 'std';\n"
+		"import * as os from 'os';\n"
+		"globalThis.std = std;\n"
+		"globalThis.os = os;\n";
+	eval_buf(ctx, str, strlen(str), "<input>", JS_EVAL_TYPE_MODULE);
+#endif
+#endif
+	return ctx;
 }
 
 static void register_helpers(RLang *lang) {
 	// TODO: move this code to init/fini
-	if (ctx != NULL || is_init) {
+	if (ctx || is_init) {
 		return;
 	}
 	is_init = true;
 	Gcore = lang->user;
-	rt = JS_NewRuntime();
-	ctx = JS_NewContext (rt);
+	rt = JS_NewRuntime ();
+#if QJS_LIBC
+	js_std_set_worker_new_context_func (JS_NewCustomContext);
+	js_std_init_handlers (rt);
+	ctx = JS_NewCustomContext (rt);
+	JS_SetModuleLoaderFunc(rt, NULL, js_module_loader, NULL);
+#else
+	ctx = JS_NewCustomContext (rt);
+#endif
 	JSModuleDef *m = JS_NewCModule (ctx, "r2", js_r2_init);
 	if (!m) {
 		return ;
 	}
 	js_r2_init (ctx, m);
-	JS_AddModuleExportList(ctx, m, js_r2_funcs, countof(js_r2_funcs));
-#if QJS_LIBC
-	if (r_config_get (Gcore->config, "cfg.sandbox") == 0) {
-		js_init_module_std(ctx, "std");
-		js_init_module_os(ctx, "os");
-	}
-#endif
+	JS_AddModuleExportList (ctx, m, js_r2_funcs, countof (js_r2_funcs));
 #if 0
 	eval(ctx, "function dir(x){"
-		"console.log(JSON.stringify(x).replace(/,/g,',\\n '));"
-		"for(var i in x) {console.log(i);}}");
+			"console.log(JSON.stringify(x).replace(/,/g,',\\n '));"
+			"for(var i in x) {console.log(i);}}");
 #endif
 }
 
@@ -138,12 +164,13 @@ static void eval_jobs(JSContext *ctx) {
 
 static bool eval(JSContext *ctx, const char *code) {
 	JSValue v = JS_Eval (ctx, code, strlen (code), "-", 0);
-	if (JS_IsException(v)) {
+	if (JS_IsException (v)) {
 		js_std_dump_error (ctx);
 		JSValue e = JS_GetException (ctx);
-		js_dump_obj(ctx, stderr, e);
+		js_dump_obj (ctx, stderr, e);
 	}
 	eval_jobs (ctx);
+	JS_FreeValue (ctx, v);
 	return true;
 }
 
