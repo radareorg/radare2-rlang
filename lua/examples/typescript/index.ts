81943a7416032d602d586a149d4e852b722ac37f
pancake <pancake@nopcode.org>
2022-11-22 03:53:22 +0100

    Simple example transipiling typescript to lua for r2

---
+++ lua/examples/typescript/index.ts
@@ -0,0 +1,7 @@
+import { R2Pipe } from "../../../quickjs/examples/ts/types/r2pipe";
+
+function main(r2: R2Pipe) {
+	console.log("Hello World");
+	console.log("Hello World");
+	// console.log(r2.cmd("?E Hello"))
+}
