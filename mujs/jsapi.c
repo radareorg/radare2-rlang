59fef8f0b75a996d3eb52bbc03b73bf1b055e330
pancake <pancake@nopcode.org>
2022-12-08 22:08:32 +0100

    Implement fs and b64 apis for the mujs plugin

--- mujs/jsapi.c
+++ mujs/jsapi.c
@@ -1,25 +1,32 @@
 /* lang.mujs plugin for r2 - 2022 - pancake */
 
-static const char *r2_js =
-	"var r2 = {cmd: r2cmd, cmdj: function(x) {\n"
+static const char *const r2_js =
+	"var r2 = {cmd: r2cmd, call: r2call, cmdj: function(x) {\n"
 	"  return JSON.parse(r2cmd(x));\n"
 	" }\n"
 	"};\n"
 ;
 static const char *require_js =
+// XXX inspect or dump !
 	"function inspect(x) { try { var o = JSON.parse(x); } catch (e) { o = x; }\nreturn JSON.stringify(o,null, 2); }\n"
+	"var dump = inspect;\n"
 	"var exports = {}\n"
 	"function require(name) {\n"
 	"var cache = require.cache;\n"
 	"if (name in cache) return cache[name];\n"
 	"var exports = {};\n"
 	"cache[name] = exports;\n"
-	"Function('exports', read(name+'.js'))(exports);\n"
+	"Function('exports', readFileSync(name+'.js'))(exports);\n"
 	"return exports;\n"
 	"}\n"
 	"require.cache = Object.create(null);\n"
 ;
 
+static const char *const fs_js =
+	"var fs = {}\n"
+	"fs.readFileSync = readFileSync;\n"
+	"fs.writeFileSync = writeFileSync;\n"
+;
 
 static const char *stacktrace_js =
 	"Error.prototype.toString = function() {\n"
@@ -33,9 +40,18 @@ static const char *console_js =
 	"var console = { log: print, debug: print, warn: print, error: print };"
 ;
 
+static void jsB_write(js_State *J) {
+	const char *filename = js_tostring(J, 1);
+	const char *data = js_tostring(J, 2);
+	bool append = false; // TODO fs.writeFileSync(path, data,{flag:'a+'});   //'a+' is append mode
+	r_file_dump (filename, (const ut8*)data, strlen (data), append);
+	js_pushundefined(J);
+}
+
 static void jsB_read(js_State *J) {
 	// TODO: check r2 sandbox
-	const char *filename = js_tostring(J, 1);
+	const char *filename = js_tostring (J, 1);
+#if 0
 	FILE *f;
 	char *s;
 	int n, t;
@@ -74,10 +90,19 @@ static void jsB_read(js_State *J) {
 		js_error(J, "cannot read data from file '%s': %s", filename, strerror(errno));
 	}
 	s[n] = 0;
-
 	js_pushstring(J, s);
 	free(s);
 	fclose(f);
+#else
+	size_t sz = 0;
+	char *data = r_file_slurp (filename, &sz);
+	if (data) {
+		js_pushstring (J, data);
+		free (data);
+	} else {
+		js_error (J, "cannot read data from file '%s': %s", filename, strerror (errno));
+	}
+#endif
 }
 
 static void jsB_print(js_State *J) {
@@ -97,4 +122,27 @@ static void jsB_gc(js_State *J) {
 	int report = js_toboolean(J, 1);
 	js_gc(J, report);
 	js_pushundefined(J);
+}
+
+static const char *const base64_js =
+	"var Base64 = { encode : b64, decode: function(x) { return b64(x,1); }\n"
+;
+
+static void jsB_b64(js_State *J) {
+	const char *input = js_tostring (J, 1);
+	bool decode = js_toboolean (J, 2);
+	if (decode) {
+		int res = 0;
+		ut8 *r = sdb_decode (input, &res);
+		if (r) {
+			js_pushstring (J, (const char *)r);
+			free (r);
+		} else {
+			js_error (J, "Invalid base64 string");
+		}
+	} else {
+		char *r = sdb_encode ((const ut8*) input, -1);
+		js_pushstring (J, r);
+		free (r);
+	}
 }
--- mujs/plugin.c
+++ mujs/plugin.c
@@ -16,7 +16,9 @@ typedef struct {
 static bool mujs_run(RLangSession *s, const char *code, int len) {
 	MujsContext *ctx = s->plugin_data;
 	js_State *J = ctx->J;
-	char *nc = r_str_newf ("try { var res = (%s); if (res != undefined) { console.log(res); } } catch(e) { console.error(e); }", code);
+	// char *nc = r_str_newf ("try { var res = (function(){%s;})(); if (res != undefined) { console.log(res); } } catch(e) { console.error(e); }", code);
+	const char *echo = strstr (code, "=")? "": "if(res!=undefined){console.log(res);}";
+	char *nc = r_str_newf ("try { var res = (%s); %s } catch(e) { console.error(e); }", code, echo);
 	js_dostring (J, nc);
 	free (nc);
 	return true;
@@ -40,6 +42,17 @@ static void r2cmd(js_State *J) {
 	}
 }
 
+static void r2call(js_State *J) {
+	MujsContext *ctx = J->uctx;
+	const char *s = js_tostring (J, 1);
+	if (s) {
+		r_core_cmd_call (ctx->core, s);
+		js_pushundefined (J);
+	} else {
+		js_error(J, "string expected %s: %s", s, strerror (errno));
+	}
+}
+
 static bool fini(RLangSession *s) {
 	MujsContext *ctx = s->plugin_data;
 	js_freestate (ctx->J);
@@ -49,20 +62,30 @@ static bool fini(RLangSession *s) {
 
 static void *init(RLangSession *s) {
 	js_State *J = js_newstate (NULL, NULL, JS_STRICT);
-
+	if (!J) {
+		return NULL;
+	}
+	js_newcfunction (J, jsB_b64, "b64", 1);
+	js_setglobal (J, "b64");
 	js_newcfunction (J, r2cmd, "r2cmd", 1);
 	js_setglobal (J, "r2cmd");
+	js_newcfunction (J, r2call, "r2call", 1);
+	js_setglobal (J, "r2call");
 
-	js_dostring(J, r2_js);
-	js_dostring(J, require_js);
-	js_dostring(J, stacktrace_js);
+	js_newcfunction (J, jsB_read, "readFileSync", 1);
+	js_setglobal (J, "readFileSync");
+	js_newcfunction (J, jsB_write, "writeFileSync", 1);
+	js_setglobal (J, "writeFileSync");
+	js_dostring (J, fs_js);
 
-	js_newcfunction(J, jsB_print, "print", 0);
-	js_setglobal(J, "print");
-	js_dostring(J, console_js);
+	js_dostring (J, r2_js);
+	js_dostring (J, require_js);
+	js_dostring (J, stacktrace_js);
+	js_dostring (J, "var global = {}; var G = global;");
 
-	js_newcfunction(J, jsB_read, "read", 1);
-	js_setglobal(J, "read");
+	js_newcfunction (J, jsB_print, "print", 0);
+	js_setglobal (J, "print");
+	js_dostring (J, console_js);
 
 	js_newcfunction(J, jsB_gc, "gc", 0);
 	js_setglobal(J, "gc");
