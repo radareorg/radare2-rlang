0550d916295d94446b0e3dd451ce2ad47892d321
pancake <pancake@nopcode.org>
2025-09-03 14:09:50 +0200

    Support local radare2 installations

--- python/build.mk
+++ python/build.mk
@@ -5,6 +5,12 @@ lang_python.${EXT_SO}:
 	$(shell PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" pkg-config --cflags --libs r_reg r_core r_cons) \
 	${LDFLAGS_LIB} -o lang_python.${EXT_SO} python.c -lpython37
 else
+# Prefer local radare2 headers and pkg-config if available
+R2SRC?=./radare2
+R2_LOCAL_INC:=$(R2SRC)/libr/include
+R2_LOCAL_PKG1:=$(R2SRC)/libr/pkgconfig
+R2_LOCAL_PKG2:=$(R2SRC)/pkgcfg
+
 PYCFLAGS=$(shell PYVER=3 ./python-config-wrapper --includes) -DPYVER=3
 # Python3.8+ requires new `--embed` flag to include `-lpython`. 
 PYLDFLAGS=$(shell PYVER=3 ./python-config-wrapper --libs --embed || \
@@ -12,7 +18,11 @@ PYLDFLAGS=$(shell PYVER=3 ./python-config-wrapper --libs --embed || \
 PYLDFLAGS+=$(shell PYVER=3 ./python-config-wrapper --ldflags)
 PYLDFLAGS+=${LDFLAGS_LIB}
 
+# Add local radare2 headers if present
 CFLAGS+=-g
+ifneq (,$(wildcard $(R2_LOCAL_INC)/r_core.h))
+CFLAGS+=-I$(R2_LOCAL_INC)
+endif
 CFLAGS+=$(PYCFLAGS)
 CFILES=python.c
 ifeq ($(shell pkg-config --max-version 5.8.8 r_core && echo 1),1)
@@ -23,7 +33,7 @@ OFILES=$(subst .c,.o,${CFILES})
 
 lang_python.$(EXT_SO): $(OFILES)
 	${CC} $(CFILES) ${CFLAGS} ${PYLDFLAGS} \
-	$(shell PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" pkg-config --cflags --libs r_reg r_core r_cons) \
+	$(shell PKG_CONFIG_PATH="$(R2_LOCAL_PKG1):$(R2_LOCAL_PKG2):$(PKG_CONFIG_PATH)" pkg-config --cflags --libs r_reg r_core r_cons) \
 	${LDFLAGS} ${LDFLAGS_LIB} -fPIC -o lang_python.$(EXT_SO)
 endif
 
